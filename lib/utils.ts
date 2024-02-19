df82e7c46bd6f2aa7365e88e5401438fea05df9e
ChenJW <2774721329@qq.com>
2024-02-19 13:41:47 +0800

    feat:  Add debounce and throttle functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,45 @@
 const isArrayOfStrings = (value: unknown): value is string[] => {
     return Array.isArray(value) && value.every(item => typeof item === "string");
 }
-export { isArrayOfStrings }
+
+// 防抖函数
+const debounce = (fn: any, delay: number) => {
+    let timer: any = null;
+    return function () {
+        if (timer) { clearTimeout(timer); }
+        timer = setTimeout(() => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            // eslint-disable-next-line prefer-rest-params
+            fn.apply(this, arguments);
+            timer = null;
+        }, delay);
+    };
+};
+
+// 节流函数
+const throttle = (fn: any, delay: number) => {
+    let timer: any = null;
+    let status = false;
+    return function () {
+        if (timer) { return; }
+        if (!status) {
+            status = true;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            // eslint-disable-next-line prefer-rest-params
+            fn.apply(this, arguments);
+        }
+        timer = setTimeout(() => {
+            if (status) {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line prefer-rest-params
+                fn.apply(this, arguments);
+                timer = null;
+            }
+        }, delay);
+    };
+};
+
+export { isArrayOfStrings, debounce, throttle }
